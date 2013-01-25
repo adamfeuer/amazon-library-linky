d19215a46a6039b97dd577193d1c58f4833982ac
Adam Feuer <adamf@pobox.com>
2013-01-24 21:39:07 -0800

    selecting library using options page now works; localStorage now working too

--- options.js
+++ options.js
@@ -1,8 +1,29 @@
 
+var background = chrome.extension.getBackgroundPage();
+
 $(document).ready(function() { 
-   $("#library").select2(); 
  });
 
-function saveLibrary() {
-   chrome.extension.getBackgroundPage().console.log('foo');
-};
+function save(select) {
+   var libraryServer = $('#library').val();
+   //alert('bar: ' + libraryServer);
+   if (window.localStorage == null) {
+      alert('Local storage is required for using options.');
+      return;
+    }
+    window.localStorage.libraryServer = libraryServer;
+}
+
+function main() {
+   if (window.localStorage == null) {
+      alert("LocalStorage must be enabled for using options.");
+      return;
+  }
+  //alert("foo: " + window.localStorage.libraryServer);
+  $("#library").select2().select2('val', window.localStorage.libraryServer);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+   main();
+   document.querySelector('#library-save').addEventListener('click', save);
+});
